e8f884169fe7491629b138cf8e6bba3c4c3d560c
rigred <rigo.reddig@gmail.com>
2020-09-28 13:14:45 +0200

    output messages

--- PWRctl.py
+++ PWRctl.py
@@ -24,31 +24,72 @@ STAT_PARITY   = 6 #0x110
 STAT_DATABAD  = 7 #0x111
 
 serialPort = '/dev/ttyUSB0' # change as needed depending on system
-numDev = 0 # load this value with numDevices queried from Arduino
+numDev = 4 # load this value with numDevices queried from Arduino
 
 ser = serial.Serial(serialPort, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=1)
 
-def power(devId, state):
-	if (state):
+def bitsauce(devId, act): #function that actual smushes our bitfields together as requested.
 
 
-		print('powering on')
-	else :
-		print('powering off')
+	print('bit sauce applied - sending')
+	return STAT_ON # return the status code from the arduino
+
+
+def power(devId, state): #set the requested power state
+	if (state): # Power on the system
+		if (status(devId) == STAT_OFF):
+			result = bitsauce(devId, ACT_ON)
+			if (result == STAT_ON):
+				print('system on')
+			elif (result == STAT_ON_FAIL):
+				print('system failed to power on')
+			else:
+				print('unknown error occured trying to power on the system')
+		else:
+			print('system already on')
+		
+	else: #Power off the system
+		if (status(devId) == STAT_ON):
+			result = bitsauce(devId, ACT_OFF)
+			if (result == STAT_ON):
+				print('system off')
+			elif (result == STAT_ON_FAIL):
+				print('system failed to power off')
+			else:
+				print('unknown error occured trying to power off the system - try the \'kill\' action if neccessary')
+		else:
+			print('system already off')
+	
 
 def reset(devId):
-	print('system reset')
+	result = bitsauce(devId, ACT_RST)
+	if (result == STAT_OK):
+		print('system reset')
 
 def kill(devId):
-	print('system forced off')
+	result = bitsauce(devId, ACT_KILL)
+	if (result == STAT_OFF):
+		print('system off')
+	elif (result == STAT_OFF_FAIL):
+		print('system kill failed. system still on - something went horribly wrong!')
+	else:
+		print('unknown error occurred while trying to force off the system - check the arduino')
 
 def status(devId):
-	print('system status: ')
+	result = bitsauce(devId, ACT_STAT)
+	if (result == STAT_OFF):
+		print('system off')
+	elif(result ==STAT_ON):
+		print('system on')
+	else:
+		print('invalid status received from arduino - check the arduino') 
 
-def query(): # the function to get a devices status (on/off)
-	#ser.write()
+	return result
+	
+
+def query(): # the function to get number of devices on the system
+	#bitsauce(0, ACT_QRY)
 	print('device stat')
-
 
 
 my_parser = argparse.ArgumentParser(description='Control Computers via Arduino')
@@ -87,18 +128,13 @@ if args.devnum and args.action:
 		status(args.devnum)
 
 elif args.action and not args.devnum and args.action == 'status':
-	i = 1
+	i = 0
 	while i < numDev:
 		status(i)
+		i += 1
 
 else: 
 	print('cannot perform an action without a valid device number specified')
-
-
-
-
-
-
 
 
 #print(ser.name)
@@ -106,5 +142,3 @@ else:
 #buf = ser.readline();
 #print(buf)
 #ser.close()
-
-
